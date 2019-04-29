05fa031ec27d42bd0548b371542ea84557bdadc8
HiroyukiChiba <hiroyuki_chiba@MacBook-Pro-3.local>
2019-04-30 08:47:43 +0900

    add : footerコンポーネント追加

---
+++ components/footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="footer">
+    <p class="footer__copyright">
+      &copy; 2019 Whether And Fashion
+    </p>
+  </footer>
+</template>
+
+<style>
+.footer {
+  margin-top: 68px;
+  background-color: #eee;
+}
+.footer__copyright {
+  font-size: 12px;
+  text-align: center;
+  line-height: 1;
+  padding-top: 8px;
+  padding-bottom: 8px;
+}
+</style>
