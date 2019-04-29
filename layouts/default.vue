118b2388f8a59edb94a51b471d8ee932a78a6841
HiroyukiChiba <hiroyuki_chiba@MacBook-Pro-3.local>
2019-04-30 08:49:04 +0900

    update : レイアウトファイルでheader、footerのimport

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,27 @@
 <template>
-  <div>
-    <nuxt/>
+  <div class="main">
+    <commonHeader />
+    <nuxt />
+    <commonFooter />
   </div>
 </template>
 
+<script>
+import commonHeader from '~/components/header.vue'
+import commonFooter from '~/components/footer.vue'
+
+export default {
+  components: {
+    commonHeader,
+    commonFooter
+  }
+}
+</script>
+
 <style>
 html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -14,40 +29,22 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
+  color: #666;
 }
 
-*, *:before, *:after {
+*,
+*:before,
+*:after {
   box-sizing: border-box;
   margin: 0;
+  padding: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.main {
+  width: 100%;
+  max-width: 414px;
+  margin-right: auto;
+  margin-left: auto;
+  background-color: #f7f7f7;
 }
 </style>
-
